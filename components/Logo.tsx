3526eb937d3ed49d2971a3e0caa801aab69e53c0
picfire <mcruz85@asu.edu>
2025-09-27 18:57:56 -0700

    logo update

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,31 +1,13 @@
-import Image from "next/image";
-import GlareHover from "./GlareHover";
-
 export function Logo() {
   return (
-    <div className="flex justify-center -mt-10">
-      <GlareHover
-        width="auto"
-        height="auto"
-        background="transparent"
-        borderRadius="0px"
-        borderColor="transparent"
-        glareColor="#FFF6DF"
-        glareOpacity={0.4}
-        glareAngle={-28}
-        glareSize={280}
-        transitionDuration={700}
-        className="p-0"
-        style={{ border: "0" }}
-      >
-        <Image
-          src={"logo_svg.svg"}
-          alt="SparkBite Logo"
-          width={500}
-          height={200}
-          priority
-        />
-      </GlareHover>
+    <div className="flex justify-center">
+      <img
+        src="/logo_svg.svg"
+        alt="SparkBite Logo"
+        className="h-24 md:h-32 lg:h-40 w-auto select-none"
+        decoding="async"
+        loading="eager"
+      />
     </div>
   );
 }
