24b691aaa4db47d04d42941a16e0d34c24075685
Lakshya Dhingra <dlakshya223@gmail.com>
2025-09-27 17:28:56 -0700

    Navbar addition

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
-import { RecipeFinder } from '@/components/RecipeFinder';
+import { RecipeFinder } from "@/components/RecipeFinder";
+import { Navbar } from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main>
+      <Navbar />
       <RecipeFinder />
     </main>
   );
